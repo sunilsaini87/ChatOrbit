2a0c794c6645294f6d310318dfb17f9a3ad3bd3a
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-13 22:48:56 +0530

    update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,41 +1,49 @@
 import { createContext, useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
 
 const SocketContext = createContext();
 
 export const useSocketContext = () => {
-	return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
 export const SocketContextProvider = ({ children }) => {
-	const [socket, setSocket] = useState(null);
-	const [onlineUsers, setOnlineUsers] = useState([]);
-	const { authUser } = useAuthContext();
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
 
-	useEffect(() => {
-		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
-				query: {
-					userId: authUser._id,
-				},
-			});
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("https://chat-app-yt.onrender.com", {
+        query: {
+          userId: authUser._id,
+        },
+      });
 
-			setSocket(socket);
+      setSocket(socket);
 
-			// socket.on() is used to listen to the events. can be used both on client and server side
-			socket.on("getOnlineUsers", (users) => {
-				setOnlineUsers(users);
-			});
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
 
-			return () => socket.close();
-		} else {
-			if (socket) {
-				socket.close();
-				setSocket(null);
-			}
-		}
-	}, [authUser]);
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
 
-	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+SocketContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
